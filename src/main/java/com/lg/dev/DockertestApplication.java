1c8fb6567fde1adfc2615dbbab94c207eed0618e
liug <liugang@zbj.com>
2018-11-14 12:26:56 +0800

    tomcat

--- src/main/java/com/lg/dev/DockertestApplication.java
+++ src/main/java/com/lg/dev/DockertestApplication.java
@@ -2,13 +2,19 @@ package com.lg.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DockertestApplication {
+public class DockertestApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DockertestApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(DockertestApplication.class, args);
     }
